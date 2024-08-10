d86cb95bc4b34421816a9e4508934120a77df900
toofonwang <toofonwang@futunn.com>
2024-08-10 18:31:21 +0800

    modify fzf config

--- lua/config/plugins/fzf.lua
+++ lua/config/plugins/fzf.lua
@@ -10,37 +10,38 @@ M = {
 			fzf.grep_visual({ fzf_opts = { ['--layout'] = 'default' } })
 		end, { noremap = true })
 		fzf.setup({
-			global_resume = true,
+			global_resume       = true,
 			global_resume_query = true,
-			winopts = {
-				height     = 1,
-				width      = 1,
+			winopts             = {
+				height     = 0.9,
+				width      = 0.9,
+				fullscreen = false,
+
 				preview    = {
-					layout = 'vertical',
-					scrollbar = 'float',
+					layout     = 'horizontal',
+					scrollbar  = 'float',
+					vertical   = 'down:45%', -- up|down:size
+					horizontal = 'right:60%', -- right|left:size
+					hidden     = 'nohidden',
 				},
-				fullscreen = true,
-				vertical   = 'down:45%', -- up|down:size
-				horizontal = 'right:60%', -- right|left:size
-				hidden     = 'nohidden',
 			},
-			keymap = {
+			keymap              = {
 				builtin = {
-					['<c-p>']    = 'toggle-preview',
+					['<c-p>'] = 'toggle-preview',
 					-- ['ctrl-h']	   = 'preview-page-up',
 					-- ['ctrl-l']     = 'preview-page-down',
 				},
 				fzf = {
-					['esc']        = 'abort',
-					['ctrl-i']     = 'beginning-of-line',
-					['ctrl-a']     = 'end-of-line',
-					['ctrl-k']     = 'up',
-					['ctrl-j']     = 'down',
+					['esc']    = 'abort',
+					['ctrl-i'] = 'beginning-of-line',
+					['ctrl-a'] = 'end-of-line',
+					['ctrl-k'] = 'up',
+					['ctrl-j'] = 'down',
 					-- ['ctrl-h']	   = 'preview-page-up',
 					-- ['ctrl-l']     = 'preview-page-down',
 				},
 			},
-			previewers = {
+			previewers          = {
 				head = {
 					cmd  = 'head',
 					args = nil,
@@ -54,12 +55,12 @@ M = {
 					cmd = 'man -c %s | col -bx',
 				},
 				builtin = {
-					syntax         = true,   -- preview syntax highlight?
-					syntax_limit_l = 0,      -- syntax limit (lines), 0=nolimit
+					syntax         = true, -- preview syntax highlight?
+					syntax_limit_l = 0, -- syntax limit (lines), 0=nolimit
 					syntax_limit_b = 1024 * 1024, -- syntax limit (bytes), 0=nolimit
 				},
 			},
-			files = {
+			files               = {
 				prompt       = 'Files❯ ',
 				multiprocess = true, -- run command in a separate process
 				git_icons    = true, -- show git icons?
@@ -69,7 +70,7 @@ M = {
 				rg_opts      = '--color=never --files --hidden --follow -g \'!.git\'',
 				fd_opts      = '--color=never --type f --hidden --follow --exclude .git',
 			},
-			buffers = {
+			buffers             = {
 				prompt        = 'Buffers❯ ',
 				file_icons    = true, -- show file icons?
 				color_icons   = true, -- colorize file|git icons

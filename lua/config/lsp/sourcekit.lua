06ffad7afd9f047f26f26ab7f52addbb98ab0ab0
toofonwang <toofonwang@futunn.com>
2024-08-12 22:00:50 +0800

    update sourcekit config

--- lua/config/lsp/sourcekit.lua
+++ lua/config/lsp/sourcekit.lua
@@ -20,11 +20,6 @@ L = {
 				if ftype == "objc" then
 					return "objective-c"
 				end
-
-				if ftype == "objcpp" then
-					return "objective-cpp"
-				end
-
 				return ftype
 			end,
 
@@ -33,6 +28,7 @@ L = {
 				return util.root_pattern("buildServer.json")(filename)
 					or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
 					or util.find_git_ancestor(filename)
+					or util.root_pattern("Pods", "Podfile")(filename)
 					or util.root_pattern("Package.swift")(filename)
 					or vim.fn.getcwd()
 			end,
@@ -40,7 +36,32 @@ L = {
 				completeFunctionCalls = true,
 			},
 		})
+
+		-- 将 mm 和 h 文件类型更改为 objc
+		vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+			pattern = { "*.mm", "*.h" },
+			callback = function()
+				local cwd = vim.fn.getcwd()
+				local patterns = {
+					"buildServer.json",
+					"*.xcodeproj",
+					"*.xcworkspace",
+					"Pods",
+					"Podfile",
+					"Package.swift"
+				}
+
+				for _, pattern in ipairs(patterns) do
+					local matches = vim.fn.glob(cwd .. "/" .. pattern)
+					if matches ~= "" then
+						vim.cmd [[ set filetype=objc ]]
+						vim.cmd [[ LspStart efm ]]
+					end
+				end
+			end,
+		})
 	end,
 }
+
 
 return L
